27d4642fa8793e659589164c639b817ec8efeca0
AfonsoDev <peresfon@gmail.com>
2022-12-30 18:08:51 -0300

    att

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) =>{
-    
+app.get('/games', (req, res) =>{
+    return res.json([])    
+})
+
+app.post('/ads', (req, res) =>{
+    return res.json([])    
+})
+
+app.get('/games/:id/ads', (req, res) =>{
+    const gameId = req.params.id;
+    return res.json([])  
 })
 
 app.listen(3333)
